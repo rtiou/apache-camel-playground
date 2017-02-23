34362ca638834c0ff55bd47c175de5d56316cb33
Ronaldo Lien Tiou <ronaldo@backbase.com>
2017-02-23 20:02:06 +0100

    First commit

---
+++ src/main/java/com/tiou/ApacheCamelPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package com.tiou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelPlaygroundApplication.class, args);
+	}
+}
